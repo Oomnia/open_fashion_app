9bf4562aeb6e56a200386bc615e63eff189428ac
Omnia Said <omniasaied13201@gmail.com>
2026-02-22 22:21:26 +0200

    Create The AppBar

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:open_fashion_app/core/colors.dart';
+import 'package:open_fashion_app/widgets/custom_app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: AppColors.primary,
+appBar: CustomAppBar(isBlackk: true),    );
   }
 }
--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:open_fashion_app/core/colors.dart';
 
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key, required this.isBlackk});
+  final bool isBlackk;
+
+  @override
+  Size get preferredSize => const Size.fromHeight(80);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    bool isBlack = isBlackk;
+
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: AppBar(
+        actions: [
+          SvgPicture.asset("assets/svgs/Search.svg",color: isBlack ? Colors.white : AppColors.primary),
+          Gap(20),
+          SvgPicture.asset("assets/svgs/shopping bag.svg",color: isBlack ? Colors.white : AppColors.primary),
+        ],
+        leadingWidth: 25,
+        centerTitle: true,
+        scrolledUnderElevation: 0.0,
+        backgroundColor: isBlack ? AppColors.primary : Colors.white,
+        leading: SvgPicture.asset("assets/svgs/Menu.svg",color: isBlack ? Colors.white : AppColors.primary),
+        title: SvgPicture.asset("assets/svgs/Logo.svg",color: isBlack ? Colors.white : AppColors.primary),
+      ),
+    );
   }
 }
